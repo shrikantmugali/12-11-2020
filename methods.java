6cf94486c853b8f5cac5b9257aaf4d97e48f6367
shrikantmugali <74345560+shrikantmugali@users.noreply.github.com>
2020-11-12 12:48:01 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,21 @@
+class Method{
+
+	public static void UserInfo(){
+
+		System.out.println("hello world");
+	}
+	
+
+		
+
+	public static void main(String args[]){
+
+	 Method.UserInfo();		
+
+
+}
+
+
+
+
+}
